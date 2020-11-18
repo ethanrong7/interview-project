ddfb957ddc444ad5657933e84324e83f1a68a198
Ethan Rong <ethanrong@192-168-1-102.tpgi.com.au>
2020-11-18 19:40:32 +1100

    fixed form, modified sql table

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.post("/submit", function(req, res){
         }
 
         console.log("connected");
-        var sql = "INSERT INTO application (`company_name`, `applied_position`, `year_app`) VALUES ('" + req.body.company + "', '" + req.body.companyRole + "', '" + req.body.yearpicker + "')";
+        var sql = "INSERT INTO application (`company_name`, `applied_position`, `year_app`, `type_of_interview1`, `interview1_questions`, `type_of_interview2`, `interview2_questions`, `type_of_interview3`, `interview3_questions`, `type_of_interview4`, `interview4_questions`, `yearly_salary`, `culture`) VALUES ('" + req.body.company + "', '" + req.body.companyRole + "', '" + req.body.yearpicker + "', '" + req.body.interviewType1 + "', '" + req.body.interviewQuestion1 + "', '" + req.body.interviewType2 + "', '" + req.body.interviewQuestion2 + "', '" + req.body.interviewType3 + "', '" + req.body.interviewQuestion3 + "', '" + req.body.interviewType4 + "', '" + req.body.interviewQuestion4 + "', '" + req.body.salaryResp + "', '" + req.body.cultureResp + "')";
+        
         connection.query(sql, function(err, result) {
             if (err) {
                 throw (err);
