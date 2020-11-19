5f1ac50e8b149b631762d2c679355a2158dec523
Ethan Rong <ethanrong@192-168-1-102.tpgi.com.au>
2020-11-19 19:43:15 +1100

    new file:   node_modules/braces/CHANGELOG.md ds#	new file:   node_modules/cacheable-request/package.json : wq 	new file:   node_modules/cardinal/examples/README.md

--- app.js
+++ app.js
@@ -20,8 +20,15 @@ var connection = mysql.createPool({
     database: 'applicationDB'
 });
 
-app.get("/", function(req, res){
+connection.getConnection(function(err){
+    if (err) {
+        throw (err);
+    } else {
+        console.log("Successfully connected to mySQL");
+    }
+});
 
+app.get("/", function(req, res){
     res.render("home");
 });
 
@@ -30,25 +37,23 @@ app.get("/submit", function(req, res){
 })
 
 app.post("/submit", function(req, res){
-
-    connection.getConnection(function(err){
+    var sql = "INSERT INTO application (`company_name`, `applied_position`, `year_app`, `type_of_interview1`, `interview1_questions`, `type_of_interview2`, `interview2_questions`, `type_of_interview3`, `interview3_questions`, `type_of_interview4`, `interview4_questions`, `yearly_salary`, `culture`) VALUES ('" + req.body.company + "', '" + req.body.companyRole + "', '" + req.body.yearpicker + "', '" + req.body.interviewType1 + "', '" + req.body.interviewQuestion1 + "', '" + req.body.interviewType2 + "', '" + req.body.interviewQuestion2 + "', '" + req.body.interviewType3 + "', '" + req.body.interviewQuestion3 + "', '" + req.body.interviewType4 + "', '" + req.body.interviewQuestion4 + "', '" + req.body.salaryResp + "', '" + req.body.cultureResp + "')";
+    
+    connection.query(sql, function(err, result) {
         if (err) {
             throw (err);
         }
-
-        console.log("connected");
-        var sql = "INSERT INTO application (`company_name`, `applied_position`, `year_app`, `type_of_interview1`, `interview1_questions`, `type_of_interview2`, `interview2_questions`, `type_of_interview3`, `interview3_questions`, `type_of_interview4`, `interview4_questions`, `yearly_salary`, `culture`) VALUES ('" + req.body.company + "', '" + req.body.companyRole + "', '" + req.body.yearpicker + "', '" + req.body.interviewType1 + "', '" + req.body.interviewQuestion1 + "', '" + req.body.interviewType2 + "', '" + req.body.interviewQuestion2 + "', '" + req.body.interviewType3 + "', '" + req.body.interviewQuestion3 + "', '" + req.body.interviewType4 + "', '" + req.body.interviewQuestion4 + "', '" + req.body.salaryResp + "', '" + req.body.cultureResp + "')";
-        
-        connection.query(sql, function(err, result) {
-            if (err) {
-                throw (err);
-            }
-            console.log("submitted");
-        })
+        console.log("Submitted application form");
     });
+    
     applicationNo = applicationNo + 1;
     res.render("submit");
-    
+});
+
+app.get("/company/:companyName", function(req, res) {
+    const customCompanyName = req.params.companyName;
+
+    res.render("company", {companyNameUrl: customCompanyName});
 })
 
 app.listen(3000, function() {
